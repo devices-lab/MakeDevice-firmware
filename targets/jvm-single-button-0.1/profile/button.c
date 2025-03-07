674c9166f596632e85e24523313fcf25975aac7c
james1236 <32351696+james1236@users.noreply.github.com>
2025-03-07 21:55:36 +0000

    Fix status LED pin numbers, comment out unnecessary pin definitions (button row, single button, dev rgb)

--- targets/jvm-single-button-0.1/profile/button.c
+++ targets/jvm-single-button-0.1/profile/button.c
@@ -1,6 +1,6 @@
 #include "jdprofile.h"
 
-FIRMWARE_IDENTIFIER(0x0, "JVM single button 0.1");
+FIRMWARE_IDENTIFIER(0x30aad584, "JM dual button 0.1");
 
 void app_init_services() {
     button_init(PA_4, 0, -1);
