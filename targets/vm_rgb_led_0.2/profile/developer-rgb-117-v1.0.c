2c89138172d6106a547cdf681d5eb3baf18c531b
james1236 <32351696+james1236@users.noreply.github.com>
2025-05-06 22:03:55 +0100

    Fix RGB led colors

--- targets/vm_rgb_led_0.2/profile/developer-rgb-117-v1.0.c
+++ targets/vm_rgb_led_0.2/profile/developer-rgb-117-v1.0.c
@@ -4,14 +4,14 @@ FIRMWARE_IDENTIFIER(0x33dbec8c, "JVM Developer RGB 117 v1.0");
 
 static const led_params_t params = {
     .pin_r = PA_6,
-    .pin_g = PA_7,
-    .pin_b = PA_8,
+    .pin_g = PA_8,
+    .pin_b = PA_7,
     .active_high = 0, // set to 0 for sinking current
     .led_count = 1, // how many LEDs there are
     .max_power = 20, // in mA
     .mult_r = 250, // brightness calibration
-    .mult_g = 150,
-    .mult_b = 42 
+    .mult_g = 250,
+    .mult_b = 250 
     
 };
 
